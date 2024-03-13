439d589116ae72f806d678881177251320b141cb
israt6898 <israt6898@gmail.com>
2024-03-13 15:32:37 +0600

    load data

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+    return (
+        <div>
+
+            <h1 className="text-4xl font-bold">Cafe Knowledge</h1>
+            
+        </div>
+    );
+  
+};
+
+export default Header;
